80a611c17f0bae6a8d04703fc40cb7556f838d40
Tiện Thanh <tienmt@imt-soft.com>
2023-10-20 16:39:33 +0700

    upload code

---
+++ src/app/authentication/config.js
@@ -0,0 +1,45 @@
+const mongoose = require("../../configs/connectDatabase");
+const Schema = mongoose.Schema;
+
+const accountSchema = new Schema(
+  {
+    user_name: { type: String, required: true },
+    password: { type: String, required: true },
+    role: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const roomSchema = new Schema(
+  {
+    room_name: { type: String, required: true },
+    members: [{ type: Schema.Types.ObjectId, ref: "Account" }],
+    messages: [{ type: Schema.Types.ObjectId, ref: "Message" }],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const messageSchema = new Schema(
+  {
+    sender: { type: Schema.Types.ObjectId, ref: "Account" },
+    content: { type: String, required: false },
+    image: { type: String, required: false },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Account = mongoose.model("Account", accountSchema);
+const Room = mongoose.model("Room", roomSchema);
+const Message = mongoose.model("Message", messageSchema);
+
+module.exports = {
+  Account,
+  Room,
+  Message,
+};
